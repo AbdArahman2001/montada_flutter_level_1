d8a294459b84e1598b42acd8d3048176ece1eb1c
AbedArhman <bdalrhmnalslyby710@gmail.com>
2023-01-24 14:24:55 +0200

    start functions

---
+++ lec03-functions.dart
@@ -0,0 +1,20 @@
+// checkSum(int a)=> print(a);
+checkSum(int a) {
+  print(a);
+}
+
+void main(List<String> args) {
+  compute(anon); 
+}
+
+void sum() {}
+compute(Function func) {
+   func(10);
+}
+
+final anon = (int nickname) {
+var myName = "Alberto";
+myName += nickname.toString();
+print("hello");
+};
+
