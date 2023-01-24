8e1863c4ffa938e7e9140952481dd81e446f772f
AbedArhman <bdalrhmnalslyby710@gmail.com>
2023-01-24 13:40:46 +0200

    update lec03.dart - remove redundunt code

--- lec03.dart
+++ lec03.dart
@@ -14,44 +14,6 @@
 //     "I'm seller, my age is 35, I'v buaght 120 ordres",
 //     "I'm delivery, my age is 25, I'v delivered 86 orders"]
 
-//  List<List<dynamic>> users =[[34,11],[36,12],[21,56],[52,12],[28,10],[43,2],[55,4]]; [age,orders_count]
-
-// enum User {
-//   customer,
-//   seller,
-//   delivery,
-// }
-
-// extension userInfo on User {
-//   String getUserInfo(int age, int ordersCount) {
-//     switch (this) {
-//       case User.customer:
-//         return "I'm customer, my age is $age, I'v ordered $ordersCount ordres";
-//       case User.seller:
-//         return "I'm seller, my age is $age, I'v baught $ordersCount ordres";
-//       case User.delivery:
-//         return "I'm delivery, my age is $age, I'v delivered $ordersCount ordres";
-//     }
-//   }
-// }
-
-// void main(List<String> args) {
-
-//   List<List<dynamic>> users =[[34,11],[36,12],[21,56]];
-//   User user;
-//   for(int i=0;i<users.length;i++){
-//     if(i%3 == 0){
-//       user = User.customer;
-//     }else if(i%3 == 1){
-//       user = User.seller;
-//     }else{
-//       user = User.delivery;
-//     }
-//     print(user.getUserInfo(users[i][0], users[i][1]));
-//   }
-// }
-
-//List<List<dynamic>> users =[[34,11],[36,12],[21,56],[52,12],[28,10],[43,2],[55,4]];
 
 void main() {
   List<List<int>> users = [
@@ -81,10 +43,6 @@ printUsersInfo(List<List<int>> users) {
 }
 
 enum UserType { customer, seller, driver }
-
-//I'm customer, my age is 20, I'v ordered 44 orders",
-//     "I'm seller, my age is 35, I'v buaght 120 ordres",
-//     "I'm delivery, my age is 25, I'v delivered 86 orders"
 
 extension UserInfo on UserType {
   String getUserInfo(int age, int ordersCount) {
